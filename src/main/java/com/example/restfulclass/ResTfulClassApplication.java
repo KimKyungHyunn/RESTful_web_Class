e9eeceed7d15798d68da700131b441bbe8fd05f6
KimKyunHyunn <kdkin00@gmail.com>
2021-08-07 14:48:57 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulclass/ResTfulClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulclass/ResTfulClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
